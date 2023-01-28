39a8510df94273161aac50fff37d5f6d3423f893
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-01-28 16:46:54 +0200

    contacts CLI app

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # goit-nodejs-hw-01
+
+### node index.js --action list
+
+![node index.js --action list](./screens/01.png)
+
+### node index.js --action get --id 5
+
+![node index.js --action get --id 5](./screens/02.png)
+
+### node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+![node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22](./screens/03.png)
+![add a contact in contacts.json ](./screens/04.png)
+
+### node index.js --action remove --id=3
+
+![node index.js --action remove --id=3](./screens/05.png)
+![file contacts.json after remove](./screens/06.png)
---
+++ index.js
@@ -0,0 +1,31 @@
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
+      break;
+
+    case "remove":
+      const removeCnt = await contacts.removeContact(id);
+      console.table(removeCnt);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
